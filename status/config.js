84269e4d86f3df3aa3bc85e8b4cb59712fce5af4
COMICFLY <88814871+COMICFLY@users.noreply.github.com>
2023-08-24 00:40:08 +0800

    Add files via upload

---
+++ status/config.js
@@ -0,0 +1,26 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '服务器状态监控',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    "ur2274977-439713c3ad1b790598cc3c34",
+    'ur2274977-439713c3ad1b790598cc3c34',
+  ],
+
+  // 日志天数
+  CountDays: 7,
+
+  // 是否显示检测站点的链接
+  ShowLink: false,
+
+  // 导航栏菜单
+  Navi: [
+    {
+  //      text: '首页',
+  //      url: ''
+    },
+  ],
+};
